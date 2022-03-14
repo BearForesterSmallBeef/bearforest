fb19dcbd31026be032b024d26e1498b036028f5c
Лесс МА <BearForest2006@gmail.com>
2022-03-14 18:44:45 +0300

    123

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-app.route("/")
+@app.route("/")
 def index():
     return "Привет от проиложения Фласк"
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
